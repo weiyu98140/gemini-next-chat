547c45e339a523c274d1378eed47075844f87097
cdcd72 <cdcd71517@gmail.com>
2025-06-29 01:29:26 +0800

    feat: Add support for assistant market URLs for zh-TW

--- utils/AssistantMarketUrl.ts
+++ utils/AssistantMarketUrl.ts
@@ -4,10 +4,12 @@ class AssistantMarketUrl {
     this.baseUrl = baseUrl || ''
   }
   getIndexUrl(lang: string = 'en') {
+    if (lang === 'zh-TW') return this.baseUrl + '/index.zh-TW.json'
     if (lang.startsWith('zh')) return this.baseUrl + '/index.zh-CN.json'
     return this.baseUrl
   }
   getAssistantUrl(identifier: string, lang: string = 'en') {
+    if (lang === 'zh-TW') return this.baseUrl + `/${identifier}.zh-TW.json`
     if (lang.startsWith('zh')) return this.baseUrl + `/${identifier}.zh-CN.json`
     return this.baseUrl + `/${identifier}.json`
   }
