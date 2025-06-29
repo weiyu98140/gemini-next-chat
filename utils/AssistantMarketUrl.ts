540569ed5d080a11c52c8f2d5a7731b3670c0d54
Amery2010 <amery@xiangfa.org>
2025-06-29 12:35:55 +0800

    feat: Assistant Marketplace adds multi-language support

--- utils/AssistantMarketUrl.ts
+++ utils/AssistantMarketUrl.ts
@@ -3,15 +3,15 @@ class AssistantMarketUrl {
   constructor(baseUrl?: string) {
     this.baseUrl = baseUrl || ''
   }
-  getIndexUrl(lang: string = 'en') {
-    if (lang === 'zh-TW') return this.baseUrl + '/index.zh-TW.json'
-    if (lang.startsWith('zh')) return this.baseUrl + '/index.zh-CN.json'
-    return this.baseUrl
+  getIndexUrl(lang: string = 'en-US') {
+    if (lang === 'en-US') return this.baseUrl + '/index.json'
+    if (lang === 'ar-SA') return this.baseUrl + '/index.ar.json'
+    return this.baseUrl + '/index.' + lang + '.json'
   }
-  getAssistantUrl(identifier: string, lang: string = 'en') {
-    if (lang === 'zh-TW') return this.baseUrl + `/${identifier}.zh-TW.json`
-    if (lang.startsWith('zh')) return this.baseUrl + `/${identifier}.zh-CN.json`
-    return this.baseUrl + `/${identifier}.json`
+  getAssistantUrl(identifier: string, lang: string = 'en-US') {
+    if (lang === 'en-US') return this.baseUrl + '/' + identifier + '/index.json'
+    if (lang === 'ar-SA') return this.baseUrl + '/' + identifier + '/index.ar.json'
+    return this.baseUrl + '/' + identifier + '/index.' + lang + '.json'
   }
 }
 
